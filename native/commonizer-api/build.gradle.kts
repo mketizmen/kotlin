7597a42182a000b18558d9d888bb15aceaeadae6
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-02-17 12:20:58 +0100

    fixup! [Commonizer] Implement :native:kotlin-klib-commonizer:api with support for library commonization

--- native/commonizer-api/build.gradle.kts
+++ native/commonizer-api/build.gradle.kts
@@ -14,6 +14,7 @@ publish()
 
 dependencies {
     implementation(kotlinStdlib())
+    implementation(project(":native:kotlin-native-utils"))
     testImplementation(project(":kotlin-test::kotlin-test-junit"))
     testImplementation(commonDep("junit:junit"))
     testImplementation(projectTests(":compiler:tests-common"))
